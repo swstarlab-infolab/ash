b9ee17033b24a663952d3f571464677a80a59678
Seyeon Oh(vee) <vee@nullptr.kr>
2021-05-15 15:33:00 +0900

    Add size2str functions for binary numbers

--- source/string/size2str.cpp
+++ source/string/size2str.cpp
@@ -28,4 +28,37 @@ char* size2str_decimal(uint64_t size, char* out, size_t bufsize) {
     return nullptr;
 }
 
+char* size2str_binary(uint64_t size, char* out, size_t bufsize) {
+    constexpr int dim = sizeof(size_defs_binary) / sizeof(*size_defs_binary);
+    if (size == 0) {
+        snprintf(out, bufsize, "0%s", size_defs_binary[dim - 1]);
+    }
+    uint64_t  multiplier = exbibytes;
+    for (size_t i = 0; i < dim; i++, multiplier /= 1024) {
+        if (size < multiplier)
+            continue;
+        if (size % multiplier == 0)
+            snprintf(out, bufsize, "%" PRIu64 "%s", size / multiplier, size_defs_binary[i]);
+        else
+            snprintf(out, bufsize, "%.1f%s", static_cast<float>(size) / static_cast<float>(multiplier), size_defs_binary[i]);
+        return out;
+    }
+    return nullptr;
+}
+
+char* size2str_binary(uint64_t size, size_prefix base, char* out, size_t bufsize) {
+    uint64_t  multiplier = exbibytes;
+    constexpr int dim = sizeof(size_defs_binary) / sizeof(*size_defs_binary);
+    if (size == 0) {
+        snprintf(out, bufsize, "0%s", size_defs_binary[dim - 1]);
+    }
+    for (int i = 0; i < static_cast<int>(base); ++i)
+        multiplier /= 1024;
+    if (size % multiplier == 0)
+        snprintf(out, bufsize, "%" PRIu64 "%s", size / multiplier, size_defs_binary[static_cast<int>(base)]);
+    else
+        snprintf(out, bufsize, "%.1f%s", static_cast<float>(size) / multiplier, size_defs_binary[static_cast<int>(base)]);
+    return out;
+}
+
 } // !namespace ash
