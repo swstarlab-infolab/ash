4ff0874dc0af5e528cf9fd9a1464766de5588ec9
Seyeon Oh(vee) <vee@nullptr.kr>
2021-05-15 13:41:00 +0900

    Add size2str function for decimal numbers

--- source/string/size2str.cpp
+++ source/string/size2str.cpp
@@ -10,4 +10,22 @@ static const char* size_defs_decimal[] = { " E", " P", " T", " G", " M", " K", "
 static const uint64_t  exbibytes = 1024ULL * 1024ULL * 1024ULL * 1024ULL * 1024ULL * 1024ULL;
 static const uint64_t  exabyte = 1000ULL * 1000ULL * 1000ULL * 1000ULL * 1000ULL * 1000ULL;
 
+char* size2str_decimal(uint64_t size, char* out, size_t bufsize) {
+    constexpr int dim = sizeof(size_defs_decimal) / sizeof(*size_defs_decimal);
+    if (size == 0) {
+        snprintf(out, bufsize, "0%s", size_defs_binary[dim - 1]);
+    }
+    uint64_t  multiplier = exabyte;
+    for (size_t i = 0; i < dim; i++, multiplier /= 1000) {
+        if (size < multiplier)
+            continue;
+        if (size % multiplier == 0)
+            snprintf(out, bufsize, "%" PRIu64 "%s", size / multiplier, size_defs_decimal[i]);
+        else
+            snprintf(out, bufsize, "%.1f%s", static_cast<float>(size) / static_cast<float>(multiplier), size_defs_decimal[i]);
+        return out;
+    }
+    return nullptr;
+}
+
 } // !namespace ash
