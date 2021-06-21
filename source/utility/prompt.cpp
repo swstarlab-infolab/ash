e1a2eb7ea6405f2e23fea2c20fb66b208f66f11c
Seyeon Oh(vee) <vee@nullptr.kr>
2021-06-21 17:10:00 +0900

    Add prompt

---
+++ source/utility/prompt.cpp
@@ -0,0 +1,23 @@
+#include <ash/utility/prompt.h>
+#include <iostream>
+
+namespace ash {
+
+bool yesno_prompt::operator()() {
+    std::cout << text << std::endl;
+    char c;
+    do {
+        std::cout << "prompt (y/n)> ";
+        std::cin >> c;
+    } while (!std::cin.fail() && c != 'y' && c != 'n');
+    if (c == 'y') {
+        yes = true;
+        no = false;
+    }
+    else {
+        yes = false;
+        no = true;
+    }
+    return yes;
+}
+} // namespace ash
