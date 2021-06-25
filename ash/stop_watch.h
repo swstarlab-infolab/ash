31d983075e4cc23d474158d02b65409a879cbc25
Seyeon Oh(vee) <vee@nullptr.kr>
2021-06-25 11:11:00 +0900

    Improve stop_watch

--- ash/stop_watch.h
+++ ash/stop_watch.h
@@ -12,30 +12,43 @@ public:
     using seconds = std::chrono::seconds;
     using time_point = clock::time_point;
 
-    stop_watch() : _start(clock::now()) {
+    stop_watch() : _begin(clock::now()), _lab(_begin) {
         static_assert(std::chrono::steady_clock::is_steady, "Serious OS/C++ library issues. Steady clock is not steady");
         // FYI:  This would fail  static_assert(std::chrono::high_resolution_clock::is_steady(), "High Resolution Clock is NOT steady on CentOS?!");
     }
 
+    void lab() {
+        _lab = clock::now();
+    }
+
     uint64_t elapsed_us() const {
-        return std::chrono::duration_cast<microseconds>(clock::now() - _start).count();
+        return std::chrono::duration_cast<microseconds>(clock::now() - _begin).count();
     }
 
     uint64_t elapsed_ms() const {
-        return std::chrono::duration_cast<milliseconds>(clock::now() - _start).count();
+        return std::chrono::duration_cast<milliseconds>(clock::now() - _begin).count();
     }
 
     double elapsed_sec() const {
-        return std::chrono::duration<double>(clock::now() - _start).count();
+        return std::chrono::duration<double>(clock::now() - _begin).count();
     }
 
     time_point restart() {
-        _start = clock::now();
-        return _start;
+        _begin = clock::now();
+        return _begin;
+    }
+
+    time_point const& begin() const {
+        return _begin;
+    }
+
+    time_point const& end() const {
+        return _lab;
     }
 
 private:
-    time_point _start;
+    time_point _begin;
+    time_point _lab;
 };
 
 
