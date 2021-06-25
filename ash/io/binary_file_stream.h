3b75556d4cf3b126ddb0d61dc0f4e56c8e9d9d86
Seyeon Oh(vee) <vee@nullptr.kr>
2021-06-25 13:06:00 +0900

    Add an interface of a new file stream handler

---
+++ ash/io/binary_file_stream.h
@@ -0,0 +1,38 @@
+#ifndef ASH_IO_DIRECT_STREAM_H
+#define ASH_IO_DIRECT_STREAM_H
+#include <string>
+#include <ash/config.h>
+
+namespace ash {
+
+// abstract class binary_file_stream
+class binary_file_stream {
+public:
+    struct configure_t {
+        uint32_t alignment = 0;
+    };
+
+    enum class error_code_t {
+        Success = 0,
+        Undefined = 1,
+        InvalidArgument,
+        SeekError,
+        ReadError,
+        PosixPread64Error,
+    };
+
+    using offset_t = uint64_t;
+    virtual              ~binary_file_stream() noexcept = default;
+    virtual bool         open(std::string path) = 0;
+    virtual bool         close() noexcept = 0;
+    virtual bool         is_open() const noexcept = 0;
+    virtual bool         read(void* buf, offset_t offset, size_t size) = 0;
+    virtual uint64_t     gcount() const noexcept = 0;
+    virtual char const* path() const noexcept = 0;
+    virtual error_code_t get_last_error() const noexcept = 0;
+    virtual uint64_t     get_file_size() const noexcept = 0;
+};
+
+} // namespace ash
+
+#endif // ASH_IO_DIRECT_STREAM_H
