a05e435996ec48bec94091609b83297461d1e262
Seyeon Oh(vee) <vee@nullptr.kr>
2021-08-21 13:01:00 +0900

    Add buddy_table implementation

--- source/memory/buddy_table.cpp
+++ source/memory/buddy_table.cpp
@@ -1,4 +1,12 @@
 #include <ash/memory/buddy_table.h>
+#include <ash/size.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+#include <limits>
 
 namespace ash {
 namespace buddy_impl {
@@ -46,5 +54,217 @@ struct buddy_table_size_info {
  * Note that the patterns of a first binary level (4) are assumed to be R-A3B1*
  *
  */
+
+buddy_table_size_info get_buddy_table_size_info(cof_type const root, cof_type const min_cof) {
+    // Note that a size of root node is root * alignment
+    auto const get_linear_bound = [](cof_type n, cof_type min_cof) -> cof_type {
+        do {
+            if (n & 0x1u || n <= min_cof)
+                return n;
+            n /= 2;
+        } while (true);
+    };
+
+    auto const get_binary_depth = [](cof_type odd, cof_type min_cof) -> unsigned {
+        assert(odd & 0x1u);
+        unsigned depth = 0;
+        do {
+            cof_type const q = odd / 2; // quotient
+            // select a next odd number
+            q & 0x1u ? odd = q : odd = q + 1;
+            if (q < min_cof)
+                break;
+            depth += 1;
+        } while (true);
+        return depth;
+    };
+
+    buddy_table_size_info info;
+    cof_type const linear_bound = get_linear_bound(root, min_cof);
+    unsigned const linear_depth = static_cast<unsigned>(log2u(static_cast<uint64_t>(root / linear_bound))) + 1;
+    if (linear_bound <= min_cof) {
+        info.tbl_size = info.buddy_lv = linear_depth;
+    }
+    else {
+        unsigned const binary_depth = get_binary_depth(linear_bound, min_cof);
+        info.tbl_size = linear_depth + binary_depth * 2;
+        info.buddy_lv = linear_depth + binary_depth;
+    }
+    return info;
+}
+
+buddy_table::buddy_table() {
+    _align = 0;
+    _min_cof = 0;
+    _tbl_size = 0;
+    _buddy_lv = 0;
+    memset(&_attr, 0, sizeof _attr);
+}
+
+buddy_table::~buddy_table() noexcept {
+    _free_attr(&_attr);
+}
+
+buddy_table::buddy_table(cof_type const root_cof, unsigned const align, cof_type const min_cof) :
+    buddy_table() {
+    init(root_cof, align, min_cof);
+}
+
+void buddy_table::init(cof_type const root_cof, unsigned const align, cof_type const min_cof) {
+    assert(align % 2 == 0);
+    assert(min_cof > 0);
+    auto const info = get_buddy_table_size_info(root_cof, min_cof);
+    _align = align;
+    _min_cof = min_cof;
+    _tbl_size = info.tbl_size;
+    _buddy_lv = info.buddy_lv;
+    _attr = _init_attr(_tbl_size, root_cof, align, min_cof);
+    //TODO: Add exception handling when initialization fails (11/05/2019)
+}
+
+void buddy_table::clear() noexcept {
+    _free_attr(&_attr);
+    _align = 0;
+    _min_cof = 0;
+    _tbl_size = 0;
+    _buddy_lv = 0;
+    memset(&_attr, 0, sizeof _attr);
+}
+
+void buddy_table::printout() const {
+    for (level_t i = 0; i < _tbl_size; ++i) {
+        printf("%4u | %4u | %" PRId64 "\n",
+            i, _attr.level_v[i], _attr.cof_v[i]
+        );
+    }
+}
+
+blkidx_t buddy_table::best_fit(uint64_t const block_size) const {
+    assert(block_size <= static_cast<uint64_t>(_attr.cof_v[0] * _align));
+    assert(_tbl_size > 0);
+    cof_type const cof = static_cast<cof_type>((block_size + _align - 1) / _align);
+
+    for (unsigned i = _tbl_size - 1; i > 0; --i) {
+        if (_attr.cof_v[i] >= cof) {
+            assert(block_size <= static_cast<uint64_t>(_attr.cof_v[i] * _align));
+            return i;
+        }
+    }
+    return 0;
+
+    /*cof_type diff1 = std::numeric_limits<cof_type>::max();
+    blkidx_t i = _tbl_size;
+    do {
+        cof_type const diff2 = std::abs(_attr.cof_v[i - 1] - cof);
+        if (diff2 > diff1)
+            return i;
+        diff1 = diff2;
+        i -= 1;
+    } while (i > 0);
+    return i;*/
+}
+
+void buddy_table::_free_attr(tbl_attr* attr) {
+    free(attr->level_v);
+    free(attr->cof_v);
+    free(attr->prof_v);
+    memset(attr, 0, sizeof(tbl_attr));
+}
+
+buddy_table::tbl_attr buddy_table::_init_attr(
+    unsigned const tbl_size,
+    cof_type const root,
+    unsigned const align,
+    cof_type const min_cof) {
+    assert(align % 2 == 0);
+    assert(min_cof > 0);
+    assert(root > 0);
+
+    cof_type n = root;
+    tbl_attr attr;
+    memset(&attr, 0, sizeof attr);
+    attr.level_v = static_cast<level_t*>(calloc(tbl_size, sizeof(level_t)));
+    if (attr.level_v == nullptr)
+        goto lb_err;
+    attr.cof_v = static_cast<cof_type*>(calloc(tbl_size, sizeof(cof_type)));
+    if (attr.cof_v == nullptr)
+        goto lb_err;
+    attr.prof_v = static_cast<blk_prop_t*>(calloc(tbl_size, sizeof(uint8_t)));
+    if (attr.prof_v == nullptr)
+        goto lb_err;
+
+    // root
+    attr.level_v[0] = 0;
+    attr.cof_v[0] = n;
+    attr.prof_v[0].flags |= UniqueBuddyBlock;
+    attr.prof_v[0].dist = 0;
+    attr.prof_v[0].offset = 0;
+
+    do {
+        // linear
+        blkidx_t i = 1;
+        while (i < tbl_size && !(n & 0x1u)) {
+            attr.level_v[i] = i;
+            attr.cof_v[i] = n / 2;
+            attr.prof_v[i].flags |= UniqueBuddyBlock;
+            attr.prof_v[i].dist = 1;
+            attr.prof_v[i].offset = 0;
+            n /= 2;
+            i += 1;
+        }
+
+        if (i >= tbl_size)
+            break;
+
+        assert(((tbl_size - i) & 0x1u) == 0);
+        // binary
+        level_t lv = i;
+        blkidx_t const linear_size = i;
+        bool a1b3_pattern = false;
+        while (i + 1u < tbl_size) {
+            assert(n & 0x1u);
+            cof_type const r = n / 2;
+            cof_type const l = r + 1;
+            // left-side child
+            attr.level_v[i] = lv;
+            attr.cof_v[i] = l;
+            // right-side child
+            attr.level_v[i + 1] = lv;
+            attr.cof_v[i + 1] = r;
+            // spanning info
+            if (a1b3_pattern) {
+                attr.prof_v[i].flags = RareBuddyBlock | A1B3Pattern;
+                attr.prof_v[i + 1].flags = FrequentBuddyBlock | A1B3Pattern;
+            }
+            else {
+                attr.prof_v[i].flags = FrequentBuddyBlock | A3B1Pattern;
+                attr.prof_v[i + 1].flags = RareBuddyBlock | A3B1Pattern;
+            }
+            attr.prof_v[i].dist = 2;
+            attr.prof_v[i].offset = 0;
+            attr.prof_v[i + 1].dist = 3;
+            attr.prof_v[i + 1].offset = 1;
+            // update states for next iteration
+            a1b3_pattern = l & 0x1u;
+            a1b3_pattern ? n = l : n = r;
+            i += 2;
+            lv += 1;
+        }
+        // fix for a first binary level
+        assert(linear_size + 1 < tbl_size);
+        attr.prof_v[linear_size].dist = 1;
+        attr.prof_v[linear_size + 1].dist = 2;
+        attr.prof_v[linear_size].flags =
+            attr.prof_v[linear_size + 1].flags = RareBuddyBlock | A3B1Pattern;
+    } while (false);
+    goto lb_return;
+
+lb_err:
+    _free_attr(&attr);
+
+lb_return:
+    return attr;
+}
+
 } // !namespace _buddy_impl
 } // !namespace ash
